e6b0d8281a3a186a35d41327e21e9c585d1909c7
Andrey <tvvister@mail.ru>
2016-07-27 08:02:14 +0400

    Первый commit

--- app/src/main/java/background/mblz/yandex/com/yandexlesson/YandexApplication.java
+++ app/src/main/java/background/mblz/yandex/com/yandexlesson/YandexApplication.java
@@ -2,14 +2,71 @@ package background.mblz.yandex.com.yandexlesson;
 
 import android.app.Application;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+
 import background.mblz.yandex.com.yandexlesson.handler.CriticalSectionsManager;
 import background.mblz.yandex.com.yandexlesson.loader.CollageLoaderManager;
+import background.mblz.yandex.com.yandexlesson.model.Genre;
+import background.mblz.yandex.com.yandexlesson.model.SingerInfo;
+import background.mblz.yandex.com.yandexlesson.provider.ISingerInfoProvider;
+import background.mblz.yandex.com.yandexlesson.provider.ProviderCreator;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class YandexApplication extends Application {
+
+    public Genre[] getGenres() {
+        final Genre[][] genres = {null};
+        singerInfoProvider.getAllSingers(new Callback<SingerInfo[]>() {
+            @Override
+            public void success(SingerInfo[] singerInfos, Response response) {
+                HashMap<String, List<URL>> urls = new HashMap<String, List<URL>>();
+                for (SingerInfo si : singerInfos) {
+                    for (String genreName : si.getGenres()) {
+                        if (urls.containsKey(genreName)) {
+                            try {
+                                urls.get(genreName).add(new URL(si.getSmall()));
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+
+                genres[0] = new Genre[urls.size()];
+                int index = 0;
+                for (String genre : urls.keySet()) {
+                    URL[] array = new URL[urls.get(genre).size()];
+                    int urlIndex = 0;
+                    for (URL url : urls.get(genre)) {
+                        array[urlIndex] = url;
+                        ++urlIndex;
+                    }
+                    genres[0][index] = new Genre(genre, array);
+                    ++index;
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+        return genres[0];
+    }
+
+    public ISingerInfoProvider singerInfoProvider = ProviderCreator.createSingerInfoProvider();
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Genre[] genres = getGenres();
+
         CriticalSectionsManager.init(null); // add implementation
         CollageLoaderManager.init(null); // add implementation
     }
---
+++ app/src/main/java/background/mblz/yandex/com/yandexlesson/view/ContentFragment.java
@@ -0,0 +1,29 @@
+package background.mblz.yandex.com.yandexlesson.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import background.mblz.yandex.com.yandexlesson.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContentFragment extends Fragment {
+
+
+    public ContentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_content, container, false);
+    }
+
+}
